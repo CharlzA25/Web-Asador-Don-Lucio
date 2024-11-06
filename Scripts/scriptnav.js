c1302c6e40b2507b8d4bdbf6202d750c6646b7a4
CharlzA25 <caralbap2000@gmail.com>
2024-11-06 02:52:15 -0600

    Add files via upload

--- Scripts/scriptnav.js
+++ Scripts/scriptnav.js
@@ -1,10 +1,13 @@
+// Selecciona el menú y el botón
 let navbar1 = document.querySelector('.navbar1');
+let menuBtn = document.querySelector('#menus-btn');
 
-document.querySelector('menus-btn').onclick = () =>{
+// Activa el menú cuando se hace clic en el botón de menú
+menuBtn.onclick = () => {
     navbar1.classList.toggle('active');
 }
 
+// Cierra el menú al hacer scroll
 window.onscroll = () => {
     navbar1.classList.remove('active');
-    
 }
